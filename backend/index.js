cd8dac485c0b318dca737ea070d35dae21cc8990
tahir81018 <tahir81018@gmail.com>
2024-06-21 11:31:05 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -8,40 +8,35 @@ const router = require("./Routes/index");
 const cookieParser = require("cookie-parser");
 const port = 5000;
 
-const corsOptions = {
-  origin: "https://internshala-clone-zeta.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "https://internshala-clone-zeta.vercel.app",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(__dirname + "/public"));
 
-// app.use("/", (req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use("/", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  console.log('middleware');
+  next();
+});
 
-router.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-   });
 
-// app.get("/", (req, res) => {
-//   res.send({ messsage: "Hello This is My backend" });
-// });
+
+app.get("/", (req, res) => {
+  res.send({ messsage: "Hello This is My backend" });
+});
 
 app.use("/api", router);
 
