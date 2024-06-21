8a0f7e420e1e1ec51a4c5abb5896527bd005374d
tahir81018 <tahir81018@gmail.com>
2024-06-21 10:41:06 +0530

    db

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const url = process.env.DATABASEURL;
+const url =
+  "mongodb+srv://admin:admin@cluster0.j7lxibh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 module.exports.connect = () => {
   try {
     mongoose.connect(url, console.log("Database is connected"));
