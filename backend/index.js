cca7cffc294f2c6a5eea4e2b8ca35b80950cc14d
tahir81018 <tahir81018@gmail.com>
2024-06-21 08:02:39 +0530

    changed cors

--- backend/index.js
+++ backend/index.js
@@ -8,24 +8,24 @@ const router = require("./Routes/index");
 const cookieParser = require("cookie-parser");
 const port = 5000;
 
-const corsOptions = {
-  origin: process.env.CLIENT_BASE_URL,
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: process.env.CLIENT_BASE_URL,
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(__dirname + "/public"));
 
-// app.use((req, res, next) => {
-//   req.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
+app.use((req, res, next) => {
+  req.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use("/api", router);
 
