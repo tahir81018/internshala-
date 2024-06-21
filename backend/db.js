4ad437d8863b44ac61f8f822bdbb02edb056eb17
tahir81018 <tahir81018@gmail.com>
2024-06-21 10:38:30 +0530

    db

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const url = process.env.DATABASEURL;
 module.exports.connect = () => {
   try {
-    mongoose.connect(
-      process.env.DATABASEURL,
-      console.log("Database is connected")
-    );
+    mongoose.connect(url, console.log("Database is connected"));
   } catch (err) {
     console.error(err);
     console.log("Unable to connect with database");
