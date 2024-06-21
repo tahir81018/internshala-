23e0e1096246d469f90e7228b36760cb57dad690
tahir81018 <tahir81018@gmail.com>
2024-06-21 09:07:35 +0530

    allowed origin

--- backend/index.js
+++ backend/index.js
@@ -21,15 +21,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(__dirname + "/public"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use("/api", cors());
 app.use("/api", router);
 
 // app.get("/", (req, res) => {
