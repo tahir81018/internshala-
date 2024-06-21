6a149803706f6d7ddbf6000f12ae4eb6941e68bb
tahir81018 <tahir81018@gmail.com>
2024-06-21 11:35:04 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use("/", (req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  console.log('middleware');
+  console.log('middlewares');
   next();
 });
 
