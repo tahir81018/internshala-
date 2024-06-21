826c6a65e181ec583bb546d4c2485ce8e424c9d5
tahir81018 <tahir81018@gmail.com>
2024-06-21 08:09:17 +0530

    allow origin

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ const router = require("./Routes/index");
 const cookieParser = require("cookie-parser");
 const port = 5000;
 
-// const corsOptions = {
-//   origin: process.env.CLIENT_BASE_URL,
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: "https://internshala-clone-zeta.vercel.app/",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
