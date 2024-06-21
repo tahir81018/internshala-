0c26836fa00b45ba6f564ea7b948b8fd97ff86c6
tahir81018 <tahir81018@gmail.com>
2024-06-21 11:41:11 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,12 @@ const router = require("./Routes/index");
 const cookieParser = require("cookie-parser");
 const port = 5000;
 
-// const corsOptions = {
-//   origin: "https://internshala-clone-zeta.vercel.app",
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: "https://internshala-clone-zeta.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
@@ -22,15 +22,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(__dirname + "/public"));
 
-app.use("/", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  console.log('middlewares');
-  next();
-});
+// app.use("/", (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   console.log('middlewares');
+//   next();
+// });
 
 
 
